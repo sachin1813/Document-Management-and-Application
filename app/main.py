31a57a1ece29f693003cd8a44b23c20776a85c15
sachin singh <rockstar15sachin@gmail.com>
2025-05-13 00:30:51 +0530

    middleware security addition and token validation implementation X-New-Token

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
 from app.routes import user,auth,document,ingestion
 from app.middleWare.combined import CombinedMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(CombinedMiddleware,expose_headers=["X-New-Token"])
+#for rexposing new token
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["X-New-Token"]
+)
 
-#sRegister route modules
+# custom middleware
+app.add_middleware(CombinedMiddleware)
+
+#Register route modules
 app.include_router(user.router, prefix="/users", tags=["User Management"])
 app.include_router(auth.router, prefix="/login", tags=["Authentication"])
 app.include_router(document.router, prefix="/documents", tags=["Document Management"])
